13b5d6fdeb733cf3e9201eb58f369cf2e54ccda2
Jens Fudickar <jens.fudickar@oratool.de>
2023-02-08 00:24:55 +0100

    Updated Documentation

--- documentation/models/buildPumlImages.cmd
+++ documentation/models/buildPumlImages.cmd
@@ -1,11 +1,26 @@
 java -jar ..\..\plantuml\plantuml.jar 	flow\mainflow.puml -png
 java -jar ..\..\plantuml\plantuml.jar 	flow\mainflow.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	definition\configuration.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	definition\configuration.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\converterdefinition.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\converterdefinition.puml -svg
 
 java -jar ..\..\plantuml\plantuml.jar 	listfile\listfile.puml -png
 java -jar ..\..\plantuml\plantuml.jar 	listfile\listfile.puml -svg
 
 java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\curlauthentication.puml -png
 java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\curlauthentication.puml -svg
+
+java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\jsonplaceholdermodel.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\jsonplaceholdermodel.puml -svg
+
+java -jar ..\..\plantuml\plantuml.jar 	maincomponents\maincomponents.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	maincomponents\maincomponents.puml -svg
+
+java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\globalconfigurationfile.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\globalconfigurationfile.puml -svg
+
+java -jar ..\..\plantuml\plantuml.jar 	parameterfile\parameterfile.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	parameterfile\parameterfile.puml -svg
+
+java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\curlparameterfile.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\curlparameterfile.puml -svg
