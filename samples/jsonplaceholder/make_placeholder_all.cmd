d5a7144d926e275cd47eb55b2a7eda1c7070cefa
Jens Fudickar <jens.fudickar@oratool.de>
2023-02-08 00:02:20 +0100

    jsonplaceholder samples updated to be used in documentation

--- samples/jsonplaceholder/make_placeholder_all.cmd
+++ samples/jsonplaceholder/make_placeholder_all.cmd
@@ -1,2 +1,2 @@
-json2puml /parameterfile:placeholder_parameter_curl.json /curlparameter:id=1
-json2puml /parameterfile:placeholder_parameter_curl.json /curlparameter:id=3
+json2puml /parameterfile:placeholder_parameter_curl.json /curlparameter:userid=1
+json2puml /parameterfile:placeholder_parameter_curl.json /curlparameter:userid=3
