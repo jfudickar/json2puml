fed2fb7ebea2d3ca270b20ad1d3e60384ec9a41e
Jens Fudickar <jens.fudickar@oratool.de>
2023-02-11 16:49:23 +0100

    Sample scripts to show swapi without curl

--- samples/swapi/data/make_swapi_local.cmd
+++ samples/swapi/data/make_swapi_local.cmd
@@ -1,1 +1,3 @@
 json2puml /inputlistfile:swapi_inputlist_local.json
+json2puml /inputlistfile:swapi_inputlist_local.json /summaryfile:swapi_han_solo /titlefilter:"Han Solo"
+json2puml /inputlistfile:swapi_inputlist_local.json /summaryfile:swapi_death_star /titlefilter:"Death Star"
