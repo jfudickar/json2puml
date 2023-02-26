20b9ad7d4b2cc384a6c1401d916e919ce331bf1e
Jens Fudickar <jens.fudickar@oratool.de>
2023-02-26 20:32:46 +0100

    Improved Setup Script, Setup is automatically started after build

--- installation/build_setup.cmd
+++ installation/build_setup.cmd
@@ -1,3 +1,9 @@
 call prepare_docker_dir.cmd
 call build_docker.cmd
 "c:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc json2pumlsetup.iss
+
+set EXE='..\source\win64\release\json2puml.exe'
+for /f %%i in ('powershell "(Get-Item -path %EXE%).VersionInfo.FileVersion"') do set ver=%%i
+echo %ver%
+
+output\Json2Puml.setup.%ver%.exe /silent
