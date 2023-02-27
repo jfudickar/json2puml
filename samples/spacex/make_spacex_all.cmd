e231aa12c49e640a6a8bdfc43f1bbddc81f76714
Jens Fudickar <jens.fudickar@oratool.de>
2023-02-27 21:58:15 +0100

    Updated Sample Definitions

--- samples/spacex/make_spacex_all.cmd
+++ samples/spacex/make_spacex_all.cmd
@@ -1,3 +1,15 @@
-json2puml /parameterfile:spacex_parameter_launches.json /curlparameter:launch=next /formatdefinitionfiles
-json2puml /parameterfile:spacex_parameter_launches.json /curlparameterfile:spacex_curl_parameter_launches_latest.json
-json2puml /parameterfile:spacex_parameter_launches.json /curlparameter:launch=5eb87cd9ffd86e000604b32a
+json2puml /parameterfile:spacex_parameter_launches.json /option:full /curlparameter:launch=next /formatdefinitionfiles
+json2puml /parameterfile:spacex_parameter_launches.json /option:full /curlparameterfile:spacex_curl_parameter_launches_latest.json /formatdefinitionfiles /outputformat:svg,png,zip,filelist,log
+json2puml /parameterfile:spacex_parameter_launches.json /option:full /curlparameter:launch=5eb87cd9ffd86e000604b32a
+
+json2puml /parameterfile:spacex_parameter_launches.json /option:default /curlparameter:launch=next 
+json2puml /parameterfile:spacex_parameter_launches.json /option:default /curlparameterfile:spacex_curl_parameter_launches_latest.json /outputformat:svg,png,zip,filelist,log
+json2puml /parameterfile:spacex_parameter_launches.json /option:default /curlparameter:launch=5eb87cd9ffd86e000604b32a
+
+json2puml /parameterfile:spacex_parameter_launches.json /option:compact /curlparameter:launch=next /formatdefinitionfiles
+json2puml /parameterfile:spacex_parameter_launches.json /option:compact /curlparameterfile:spacex_curl_parameter_launches_latest.json /outputformat:svg,png,zip,filelist,log
+json2puml /parameterfile:spacex_parameter_launches.json /option:compact /curlparameter:launch=5eb87cd9ffd86e000604b32a
+
+json2puml /parameterfile:spacex_parameter_launches.json /option:launch-crew-rocket /curlparameter:launch=next /formatdefinitionfiles
+json2puml /parameterfile:spacex_parameter_launches.json /option:launch-crew-rocket /curlparameterfile:spacex_curl_parameter_launches_latest.json /outputformat:svg,png,zip,filelist,log
+json2puml /parameterfile:spacex_parameter_launches.json /option:launch-crew-rocket /curlparameter:launch=5eb87cd9ffd86e000604b32a
