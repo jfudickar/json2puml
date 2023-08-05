8a974140ed6efadf11aeb9b5e53b7c660dfc1fd0
Jens Fudickar <jens.fudickar@oratool.de>
2023-08-05 22:59:04 +0200

    Docker build script improved

--- installation/build_docker.cmd
+++ installation/build_docker.cmd
@@ -2,6 +2,10 @@ set EXE='..\source\win64\release\json2puml.exe'
 for /f %%i in ('powershell "(Get-Item -path %EXE%).VersionInfo.FileVersion"') do set ver=%%i
 echo %ver%
 
+rmdir /s /q ..\docker\src\json2puml\samples
+dir ..\docker\src\json2puml\samples\*.* /s
+xcopy ..\samples\*.json ..\docker\src\json2puml\samples /s /e /i /y
+
 rmdir /s /q docker
 mkdir docker
 xcopy ..\docker\* docker /s /e /i /y
@@ -10,6 +14,7 @@ rmdir /s /q docker\logs
 rmdir /s /q docker\output
 del docker\.gitignore /s
 del docker\.dockerignore /s
+del docker\*formatted.json /s
 del docker\*.log /s
 del docker\*.url /s
 cd docker
