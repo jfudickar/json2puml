dcf93c28e856323539958bb8610ea5ff91962903
Jens Fudickar <jens.fudickar@oratool.de>
2023-08-17 23:18:15 +0200

    TJson2PumlLoggerProLogWriter added which reassigns the MVCFramework logging to the default logging, which prevents that additional logfiles are created.

--- source/json2pumlservicewebmodule.pas
+++ source/json2pumlservicewebmodule.pas
@@ -1,26 +1,26 @@
-{-------------------------------------------------------------------------------
+{ -------------------------------------------------------------------------------
 
-This file is part of the json2puml project.
+  This file is part of the json2puml project.
 
-Copyright (C) 2023 Jens Fudickar
+  Copyright (C) 2023 Jens Fudickar
 
-This program is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software Foundation;
-either version 3 of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 3 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program;
-if not, see http://www.gnu.org/licenses/gpl-3.0
+  You should have received a copy of the GNU General Public License along with this program;
+  if not, see http://www.gnu.org/licenses/gpl-3.0
 
-I am available for any questions/requests: jens.fudickar@oratool.de
+  I am available for any questions/requests: jens.fudickar@oratool.de
 
-You may retrieve the latest version of this file at the json2puml home page,
-located at https://github.com/jfudickar/json2puml
+  You may retrieve the latest version of this file at the json2puml home page,
+  located at https://github.com/jfudickar/json2puml
 
--------------------------------------------------------------------------------}
+  ------------------------------------------------------------------------------- }
 
 unit json2pumlservicewebmodule;
 
@@ -30,14 +30,39 @@ uses
   System.SysUtils,
   System.Classes,
   Web.HTTPApp,
-  MVCFramework;
+  MVCFramework,
+  LoggerPro;
 
 type
+  TJson2PumlLoggerProLogWriter = class(TLogWriter, ILogWriter)
+  strict private
+  protected
+  public
+    procedure Debug(const aMessage: string; const aTag: string); overload;
+    procedure Debug(const aMessage: string; const aParams: array of TVarRec; const aTag: string); overload;
+    procedure DebugFmt(const aMessage: string; const aParams: array of TVarRec; const aTag: string); deprecated;
+    procedure Error(const aMessage: string; const aTag: string); overload;
+    procedure Error(const aMessage: string; const aParams: array of TVarRec; const aTag: string); overload;
+    procedure ErrorFmt(const aMessage: string; const aParams: array of TVarRec; const aTag: string); deprecated;
+    procedure Info(const aMessage: string; const aTag: string); overload;
+    procedure Info(const aMessage: string; const aParams: array of TVarRec; const aTag: string); overload;
+    procedure InfoFmt(const aMessage: string; const aParams: array of TVarRec; const aTag: string); deprecated;
+    procedure Log(const aType: TLogType; const aMessage: string; const aTag: string); overload;
+    procedure Log(const aType: TLogType; const aMessage: string; const aParams: array of const; const aTag: string);
+        overload;
+    procedure LogFmt(const aType: TLogType; const aMessage: string; const aParams: array of const; const aTag: string);
+        deprecated;
+    procedure Warn(const aMessage: string; const aTag: string); overload;
+    procedure Warn(const aMessage: string; const aParams: array of TVarRec; const aTag: string); overload;
+    procedure WarnFmt(const aMessage: string; const aParams: array of TVarRec; const aTag: string); deprecated;
+  end;
+
   TJson2PumlWebModule = class(TWebModule)
     procedure WebModuleCreate (Sender: TObject);
     procedure WebModuleDestroy (Sender: TObject);
   private
     FMVC: TMVCEngine;
+    FLogWriter: TJson2PumlLoggerProLogWriter;
   public
     { Public declarations }
   end;
@@ -51,17 +76,18 @@ implementation
 
 uses json2pumlservicecontroller,
   System.IOUtils,
-  MVCFramework.Commons(*,
-  MVCFramework.Middleware.ActiveRecord,
-  MVCFramework.Middleware.StaticFiles,
-  MVCFramework.Middleware.Analytics,
-  MVCFramework.Middleware.Trace,
-  MVCFramework.Middleware.CORS,
-  MVCFramework.Middleware.ETag,
-  MVCFramework.Middleware.Compression*);
+  MVCFramework.Commons (*,
+    MVCFramework.Middleware.ActiveRecord,
+    MVCFramework.Middleware.StaticFiles,
+    MVCFramework.Middleware.Analytics,
+    MVCFramework.Middleware.Trace,
+    MVCFramework.Middleware.CORS,
+    MVCFramework.Middleware.ETag,
+    MVCFramework.Middleware.Compression*) , json2pumlloghandler;
 
 procedure TJson2PumlWebModule.WebModuleCreate (Sender: TObject);
 begin
+  FLogWriter := TJson2PumlLoggerProLogWriter.Create;
   FMVC := TMVCEngine.Create (Self,
     procedure(Config: TMVCConfig)
     begin
@@ -89,7 +115,7 @@ begin
       Config[TMVCConfigKey.MaxRequestSize] := IntToStr(TMVCConstants.DEFAULT_MAX_REQUEST_SIZE);
       // Server Name
       Config[TMVCConfigKey.ServerName] := 'JSON2PUML Server';
-    end);
+    end, FLogWriter);
   FMVC.AddController (TJson2PumlController);
 
 
@@ -120,6 +146,99 @@ end;
 procedure TJson2PumlWebModule.WebModuleDestroy (Sender: TObject);
 begin
   FMVC.Free;
+  FLogWriter.Free;
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Debug(const aMessage: string; const aTag: string);
+begin
+  GlobalLogHandler.Debug(aMessage, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Debug(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Debug(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.DebugFmt(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Debug(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Error(const aMessage: string; const aTag: string);
+begin
+  GlobalLogHandler.Error(aMessage, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Error(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Error(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.ErrorFmt(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Error(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Info(const aMessage: string; const aTag: string);
+begin
+  GlobalLogHandler.Info(aMessage, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Info(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Info(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.InfoFmt(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Info(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Log(const aType: TLogType; const aMessage: string; const aTag: string);
+begin
+  Log (aType, aMessage, [], aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Log(const aType: TLogType; const aMessage: string; const aParams: array of
+    const; const aTag: string);
+begin
+  case aType of
+    TLogType.Debug: GlobalLogHandler.Debug(aMessage, aParams, aTag);
+    TLogType.Info: GlobalLogHandler.Info(aMessage, aParams, aTag);
+    TLogType.Warning: GlobalLogHandler.Warn(aMessage, aParams, aTag);
+    TLogType.Error: GlobalLogHandler.Error(aMessage, aParams, aTag);
+  else
+    GlobalLogHandler.Info(aMessage, aParams, aTag);
+  end;
+end;
+
+procedure TJson2PumlLoggerProLogWriter.LogFmt(const aType: TLogType; const aMessage: string; const aParams: array of
+    const; const aTag: string);
+begin
+  Log (aType, aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Warn(const aMessage: string; const aTag: string);
+begin
+  GlobalLogHandler.Warn(aMessage, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.Warn(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Warn(aMessage, aParams, aTag);
+end;
+
+procedure TJson2PumlLoggerProLogWriter.WarnFmt(const aMessage: string; const aParams: array of TVarRec; const aTag:
+    string);
+begin
+  GlobalLogHandler.Warn(aMessage, aParams, aTag);
 end;
 
 end.
