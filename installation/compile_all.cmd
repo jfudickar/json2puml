c044e2bb7e23ad116deaa630f8fe4729c1f98c88
Jens Fudickar <jens.fudickar@oratool.de>
2024-02-07 21:33:37 +0100

    Removed error messages when compiling the linux executables

--- installation/compile_all.cmd
+++ installation/compile_all.cmd
@@ -3,7 +3,7 @@ cd ..\source\
 
 msbuild json2pumlapps.groupproj /p:config=Release /p:platform=Win32
 msbuild json2pumlapps.groupproj /p:config=Release /p:platform=Win64
-msbuild json2pumlapps.groupproj /p:config=Release /p:platform=Linux64
-
+msbuild json2puml.dproj /p:config=Release /p:platform=Linux64
+msbuild json2pumlservice.dproj /p:config=Release /p:platform=Linux64
 
 cd ..\installation\
