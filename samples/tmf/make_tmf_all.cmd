90464ce62e5ca0a0fa38e59d0eb986595b3e45c4
Jens Fudickar <jens.fudickar@oratool.de>
2024-02-07 21:53:55 +0100

    Fixed sample command file based on renamed definition file

--- samples/tmf/make_tmf_all.cmd
+++ samples/tmf/make_tmf_all.cmd
@@ -1,1 +1,1 @@
-json2puml /definitionfile:tmf_definition.json /inputfile:TMF717_sample.json /option:full /outputformat:svg  /formatdefinitionfiles
+json2puml /definitionfile:tmf-definition.json /inputfile:TMF717_sample.json /option:full /outputformat:svg  /formatdefinitionfiles
