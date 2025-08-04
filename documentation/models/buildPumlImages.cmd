de9168b47ef188e0acf222b26b8be9df5e734381
Jens Fudickar <jens.fudickar@oratool.de>
2025-08-05 00:07:44 +0200

    samples and documentation updated

--- documentation/models/buildPumlImages.cmd
+++ documentation/models/buildPumlImages.cmd
@@ -1,41 +1,41 @@
-java -jar ..\..\plantuml\plantuml.jar 	flow\mainflow.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	flow\mainflow.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	flow\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	flow\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\converterdefinition.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\converterdefinition.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	converterdefinition\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	listfile\listfile.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	listfile\listfile.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	listfile\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	listfile\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\curlauthentication.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\curlauthentication.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	curlauthentication\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\jsonplaceholdermodel.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\jsonplaceholdermodel.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	jsonplaceholder\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	maincomponents\maincomponents.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	maincomponents\maincomponents.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	maincomponents\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	maincomponents\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\globalconfigurationfile.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\globalconfigurationfile.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	globalconfigurationfile\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	parameterfile\parameterfile.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	parameterfile\parameterfile.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	parameterfile\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	parameterfile\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\curlparameterfile.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\curlparameterfile.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	curlparameterfile\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	getdefinitionfiles\getdefinitionfiles.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	getdefinitionfiles\getdefinitionfiles.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	getdefinitionfiles\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	getdefinitionfiles\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	getinputlistfiles\getinputlistfiles.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	getinputlistfiles\getinputlistfiles.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	getinputlistfiles\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	getinputlistfiles\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	json2pumlrequest\json2pumlrequest.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	json2pumlrequest\json2pumlrequest.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	json2pumlrequest\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	json2pumlrequest\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	getserviceinformation\getserviceinformation.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	getserviceinformation\getserviceinformation.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	getserviceinformation\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	getserviceinformation\*.puml -svg
 
-java -jar ..\..\plantuml\plantuml.jar 	geterrormessages\geterrormessages.puml -png
-java -jar ..\..\plantuml\plantuml.jar 	geterrormessages\geterrormessages.puml -svg
+java -jar ..\..\plantuml\plantuml.jar 	geterrormessages\*.puml -png
+java -jar ..\..\plantuml\plantuml.jar 	geterrormessages\*.puml -svg
